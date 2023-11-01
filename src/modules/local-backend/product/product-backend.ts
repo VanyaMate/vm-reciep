b759aff30e86978982ad97213e831579b9704d65
VanyaMate <buhu653@mail.ru>
2023-11-01 14:40:01 +0300

    create products-service

--- src/modules/local-backend/product/product-backend.ts
+++ src/modules/local-backend/product/product-backend.ts
@@ -3,26 +3,18 @@ import {
     ProductBackendDataGenerator,
 } from '@/modules/local-backend/product/product-backend.data-generator.ts';
 import {
-    ProductStorageService,
-} from '@/modules/local-backend/product/product-backend.storage-service.ts';
-import {
     CreateProductDto,
     UpdateProductDto,
 } from '@/modules/local-backend/product/product-backend.types.ts';
 import { SingleService, StorageService } from '@vanyamate/market-place-service';
-import products_1 from '@vanyamate/market-place-service/data/products/products_1.json';
-import products_2 from '@vanyamate/market-place-service/data/products/products_2.json';
 
 
 export class ProductBackend extends SingleService<Product, CreateProductDto, UpdateProductDto> {
     constructor () {
         super(
-            new ProductStorageService(
-                new StorageService(
-                    localStorage,
-                    'products',
-                ),
-                [ ...products_1, ...products_2 ],
+            new StorageService(
+                localStorage,
+                'products',
             ),
             new ProductBackendDataGenerator(),
             {
---
+++ src/modules/local-backend/products/products-backend.ts
@@ -0,0 +1,28 @@
+import { Product } from '@/modules/api/product/product-service.types.ts';
+import {
+    ProductStorageService,
+} from '@/modules/local-backend/product/product-backend.storage-service.ts';
+import { MultiplyService, StorageService } from '@vanyamate/market-place-service';
+import products_1 from '@vanyamate/market-place-service/data/products/products_1.json';
+import products_2 from '@vanyamate/market-place-service/data/products/products_2.json';
+
+
+export class ProductsBackend extends MultiplyService<Product> {
+    constructor () {
+        super(
+            new ProductStorageService(
+                new StorageService(
+                    localStorage,
+                    'products',
+                ),
+                [ ...products_1, ...products_2 ],
+            ),
+            {
+                options: {
+                    maxOperationsPerStep: 100,
+                    findOneFilter       : (product: Product, id: string) => product.barcode.toString() === id,
+                },
+            },
+        );
+    }
+}
