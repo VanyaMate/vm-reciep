fe5dcfac92a70c6ddff539c46041687567cd70ca
VanyaMate <buhu653@mail.ru>
2023-12-03 09:10:59 +0300

    add id to brand-type

--- src/modules/api/brand/brand-service.types.ts
+++ src/modules/api/brand/brand-service.types.ts
@@ -2,6 +2,7 @@ export type CreateBrandDto = Pick<Brand, 'title' | 'admins'>;
 export type UpdateBrandDto = Partial<Omit<Brand, 'id'>>;
 
 export type Brand = {
+    id: string;
     title: string;
     description: string;
     avatar: string;
